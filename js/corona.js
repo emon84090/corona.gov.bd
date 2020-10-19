59778e3ef91fd24b5b3a647afbca67770e5c7210
emon84090 <69768155+emon84090@users.noreply.github.com>
2020-10-19 20:44:33 +0600

    Add files via upload

---
+++ js/corona.js
@@ -0,0 +1,29 @@
+	
+$(document).ready(function(){
+     $('#pijon').owlCarousel({
+   
+          items:1,
+          margin:20,
+          autoplay:true,
+          loop:true
+     });
+   
+     $('#emon').owlCarousel({
+   
+          items:1,
+          margin:20,
+          autoplay:true,
+          loop:true
+          
+     });
+   
+          
+     
+   
+  
+   
+   
+   
+        
+   });
+   
